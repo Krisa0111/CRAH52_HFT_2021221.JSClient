2a6ef201f24e95c736465d7968c10b2efe2aa5bf
Krisa0111 <pingipongi01@gmail.com>
2022-05-01 22:37:55 +0200

    jsclient added

---
+++ CRAH52_HFT_2021221.JSClient/wwwroot/script.js
@@ -0,0 +1,18 @@
+﻿let clubss = [];
+
+fetch('http://localhost:30907/clubs')
+    .then(x => x.json())
+    .then(y => {
+        clubss = y;
+        console.log(clubss);
+        display();
+    });
+
+function display() {
+    clubss.forEach(t => {
+        document.getElementById('resultarea').innerHTML +=
+            "<tr><td>" + t.clubID + "</td><td>"
+            + t.clubName + "</td></tr>";
+
+    });
+}
